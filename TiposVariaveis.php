ba97e3350b4dfa4361d0d155f9b8c649030635b6
WLRS <154830106+WLRSD@users.noreply.github.com>
2024-04-22 02:38:26 -0300

    Adicionando os primeiros arquivos de PHP

---
+++ TiposVariaveis.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>VariaveisPHP</title>
+    </head>
+    <body>
+
+    </body>
+    <?PHP
+        $idade = 24;
+        $nome = (string)"Warlison";
+        $idade2 = 15;
+        $nome2 = "Carlos";
+        echo "Olá, mundo! Me chamo". $nome. " e tenho ". $idade. " anos! <br>";
+        echo "$nome2 tem $idade2 anos!";
+    ?>
+</html>
---
+++ ola.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>PHP</title>
+        <style>
+            h2 {
+                color: red;
+                text-shadow: 1px 1px 1px black;
+            }
+        </style>
+    </head>
+    <body>
+    <h1>Olá mundo! Estou aprendendo PHP</h1>
+    </body>
+    <?php
+        echo "<h2>Olá, <br>Mundo</h2>";
+    ?>
+ <html>
